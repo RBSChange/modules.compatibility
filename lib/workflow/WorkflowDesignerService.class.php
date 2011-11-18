9749be2b2b91f2f07f3a07e2330260bd354a13cc
Gaël PORT <gael.port@rbs.fr>
2011-11-18 14:15:56 +0100

    [DEPRECATE] workflow_WorkflowDesignerService and move it to compatibility module.

---
+++ lib/workflow/WorkflowDesignerService.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @deprecated
+ */
+class workflow_WorkflowDesignerService extends BaseService
+{
+	/**
+	 * @deprecated
+	 */
+	private static $instance;
+
+	/**
+	 * @deprecated
+	 */
+	public static function getInstance()
+	{
+		if (self::$instance === null)
+		{
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * @deprecated use workflow_WorkflowService::getInstance()->getNewDocumentInstance()
+	 */
+	public function getNewWorkflowInstance()
+	{
+		return workflow_WorkflowService::getInstance()->getNewDocumentInstance();
+	}
+
+	/**
+	 * @deprecated use workflow_PlaceService::getInstance()->getNewDocumentInstance()
+	 */
+	public function getNewPlaceInstance()
+	{
+		return workflow_PlaceService::getInstance()->getNewDocumentInstance();
+	}
+
+	/**
+	 * @deprecated use workflow_TransitionService::getInstance()->getNewDocumentInstance()
+	 */
+	public function getNewTransitionInstance()
+	{
+		return workflow_TransitionService::getInstance()->getNewDocumentInstance();
+	}
+
+	/**
+	 * @deprecated use workflow_ArcService::getInstance()->getNewDocumentInstance()
+	 */
+	public function getNewArcInstance()
+	{
+		return workflow_ArcService::getInstance()->getNewDocumentInstance();
+	}
+}
